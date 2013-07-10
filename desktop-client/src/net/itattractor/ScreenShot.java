ff176e26aed52bc46838d59e5651db024777db73
Zakirdin Lamiev <zakirdin.lamiev@iaau.edu.kg>
2013-07-10 16:54:59 +0600

    #2 Добавлена функция скриншотов

---
+++ desktop-client/src/net/itattractor/ScreenShot.java
@@ -0,0 +1,40 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.lang.*;
+import java.util.Date;
+
+public class ScreenShot extends Thread{
+    final int timeInMilisec=1000*5;
+    public void screenShot(){
+        Robot robot = null;
+        try {
+             robot = new Robot();
+        }
+        catch (AWTException e) {
+            e.printStackTrace();
+        }
+        BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        try {
+            ImageIO.write(screenShot, "JPG", new File("screen/" + new Date().toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void run(){
+        File dir = new File("screen/");
+        dir.mkdirs();
+        while(true)
+        {
+            try {
+                Thread.sleep(timeInMilisec);
+                this.screenShot();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
