e8b4a2bf7bbe980420cbd40d24619b87ed9f2e46
Beknazar Esenbek uulu <beknazar13@gmail.com>
2013-09-05 17:53:38 +0600

    #62 * test

--- desktop-client/src/test/java/net/itattractor/CommentSenderTest.java
+++ desktop-client/src/test/java/net/itattractor/CommentSenderTest.java
@@ -9,6 +9,6 @@ public class CommentSenderTest {
         Config.init();
         ConnectionProvider.createInstance("http://tracker-trac.demo.esdp.it-attractor.net/", "demo", "123");
         CommentSender commentSender = new CommentSender();
-        Assert.assertTrue(commentSender.sendComment(4, "test7 test2 test2"));
+        Assert.assertTrue(commentSender.sendComment(4, "test87 test2 test2"));
     }
 }
