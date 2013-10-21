524c93f55a7a007cbf826ddc8026786a609a60ff
Aleksei Mihailov <mihailov.alex.2011@gmail.com>
2013-10-21 17:11:30 +0600

    #91 * Убрал из скрипта сборки запуск тестов, так как jenkins не распознает результатов тестов если они запущены через shell

--- build.sh
+++ build.sh
@@ -1,27 +1,19 @@
 #!/bin/sh
-PROJECTS_HOME=~/projects
+PARENT_DIR=${PWD%/*}
+TRACKER_HOME=$(pwd)
 
 # building plugin and starting server
-TRACKER_HOME=$PROJECTS_HOME/tracker/plugin/Tracker
-cd $TRACKER_HOME
+PLUGIN_HOME=$TRACKER_HOME/plugin/Tracker
+cd $PLUGIN_HOME
 python setup.py bdist_egg
 
-rm $PROJECTS_HOME/trac-env/plugins/*.egg
+rm $PARENT_DIR/trac-env/plugins/*.egg
+cp -rf $PLUGIN_HOME/dist/*.egg $PARENT_DIR/trac-env/plugins/
 
-cp -rf $TRACKER_HOME/dist/*.egg $PROJECTS_HOME/trac-env/plugins/
+cd $TRACKER_HOME
 
-trac-admin $PROJECTS_HOME/trac-env upgrade
-tracd -p 8000 --basic-auth="${PROJECTS_HOME}/trac-env,${PROJECTS_HOME}/trac-env/.htpasswd,trac-env" $PROJECTS_HOME/trac-env &
-
-# build client and start tests
-
-PROJECTS_HOME=~/projects/tracker
-CLIENT_HOME=$PROJECTS_HOME/desktop-client
-
-cd $CLIENT_HOME
-
-mvn clean test
-
+trac-admin $PARENT_DIR/trac-env upgrade
+tracd -p 8000 --basic-auth="${PARENT_DIR}/trac-env,${PARENT_DIR}/trac-env/.htpasswd,trac-env" $PARENT_DIR/trac-env &
 
 
 
