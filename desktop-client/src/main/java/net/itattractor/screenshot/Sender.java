c2b858e748336ce84bb3fc863c2abfbff752be09
Aleksei Mihailov <mihailov.alex.2011@gmail.com>
2013-11-21 16:57:11 +0600

    #120   * Удалил консольный выхлоп

--- desktop-client/src/main/java/net/itattractor/screenshot/Sender.java
+++ desktop-client/src/main/java/net/itattractor/screenshot/Sender.java
@@ -60,7 +60,6 @@ public class Sender implements Command {
                 entity.addPart("keyboard_event_count", new StringBody(Integer.toString(screenshot.getKeyboardEventCount())));
                 httpPost.setEntity(entity);
                 httpClient.execute(httpPost);
-                System.out.println(screenshot.toString());
 
             } catch (Exception e) {
                 e.printStackTrace();
