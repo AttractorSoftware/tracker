6a20ed172b22513f9f9ae60c678e8a70ca3f78ef
gra <graoon@gmail.com>
2014-08-18 12:58:47 +0400

    добавил скрипт который удаляет старую версию траккер-плагина перед тем как скопировать новый и запускает трак

--- plugin/Tracker/upgrade.sh
+++ plugin/Tracker/upgrade.sh
@@ -1,3 +1,7 @@
 rm ~/trac-env/plugins/TracTracker-1.0dev.-py2.7.egg
+python setup.py bdist_egg
+cp -i dist/*.egg ~/trac-env/plugins/
+trac-admin ~/trac-env upgrade
 
+tracd -p 8000 --basic-auth="trac-env,../../../../trac-env/.htpasswd,Dev Env Rem" ~/trac-env -s
 
